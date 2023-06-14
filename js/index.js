df1bf71309018b2affc75bb9331b11346cd7f5c5
ofeterovskaya <42256431+ofeterovskaya@users.noreply.github.com>
2023-06-13 23:42:00 -0400

    I created footer and section with ul using DOM

--- js/index.js
+++ js/index.js
@@ -2,11 +2,11 @@ const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-// const myName = "Oksana Feterovskaya";
+// const myName = "Oksana Feterovskaya"; 
 // copyright.innerHTML = ` ${myName} &copy ${thisYear}`;
 copyright.innerHTML = ` Oksana Feterovskaya &copy ${thisYear}`;
 footer.appendChild(copyright);
-const skills = ['HTML', 'CSS', 'JavaScript', 'Adobe Visual', 'AdobePhotoshop', 'AdobeIllustrator', 'UI/UX'];
+const skills = ['HTML', 'CSS', 'JavaScript', 'Adobe Visual', 'AdobePhotoshop', 'AdobeIllustrator', 'UI/UX','Wordpress'];
 const skillsSection = document.getElementById("skills");
 const skillsList = skillsSection.querySelector('ul');
 for (let i = 0; i < skills.length; i++){
