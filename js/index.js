27be9075c072ccc6807db623fa8b1bb12d3b5529
ofeterovskaya <42256431+ofeterovskaya@users.noreply.github.com>
2023-06-13 20:41:43 -0400

    I created footer, with year and copyright, and new section Skills with list of skills using DOM methods

---
+++ js/index.js
@@ -0,0 +1,16 @@
+const today = new Date();
+const thisYear = today.getFullYear();
+const footer = document.querySelector('footer');
+const copyright = document.createElement('p');
+// const myName = "Oksana Feterovskaya";
+// copyright.innerHTML = ` ${myName} &copy ${thisYear}`;
+copyright.innerHTML = ` Oksana Feterovskaya &copy ${thisYear}`;
+footer.appendChild(copyright);
+const skills = ['HTML', 'CSS', 'JavaScript', 'Adobe Visual', 'AdobePhotoshop', 'AdobeIllustrator', 'UI/UX'];
+const skillsSection = document.getElementById("skills");
+const skillsList = skillsSection.querySelector('ul');
+for (let i = 0; i < skills.length; i++){
+    let skill = document.createElement('li');
+    skill.innerHTML = skills[i];
+    skillsList.appendChild(skill)
+}
