e7dbea680d38e669796dea5b2d28467e35ad3682
ofeterovskaya <42256431+ofeterovskaya@users.noreply.github.com>
2023-06-21 01:36:58 -0400

    Create form Submit button using DOM

--- js/index.js
+++ js/index.js
@@ -14,3 +14,44 @@ for (let i = 0; i < skills.length; i++){
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill)
 }
+//create Message Form Submit
+const messageForm = document.querySelector('form[name = "leave_message"]');
+messageForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const name = e.target.usersName.value;
+    const email = e.target.usersEmail.value;
+    const messages = e.target.usersMessage.value;
+    console.log("Name:", name);
+    console.log("Email:", email);
+    console.log("Message:", messages);
+
+//Display Messages in List
+const messageSection = document.getElementById("messages");
+const messageList = messageSection.querySelector('ul');
+const newMessage= document.createElement ('li');
+newMessage.innerHTML = `<a href = "mailto:${email}"> ${name} </a>
+    <span>wrote: ${messages}</span>`;
+
+//create Remove button
+const removeButton = document.createElement('button');
+    removeButton.innerText = 'remove';
+    removeButton.type = 'button';
+    removeButton.addEventListener('click', (e) => {
+        const entry = removeButton.parentNode;
+        entry.remove();
+    });
+  
+// //create Edit button
+// const editButton = document.createElement('button');
+//     editButton.innerText = 'edit';
+//     editButton.type = 'button';
+//     editButton.addEventListener('click', (e) => {
+//         const entry = editButton.parentNode;
+//         entry.edit();
+// });
+
+    newMessage.appendChild(removeButton);
+    //newMessage.appendChild(editButton);
+    messageList.appendChild(newMessage);
+    messageForm.reset();
+})
