bf6ea31af2a3e83ef22a0b66b17a583f6823440a
Oksana Fetter <fetter.oksana@gmail.com>
2023-07-31 23:54:28 -0400

    I added some animation styles for img, projects, and contact section

--- js/index.js
+++ js/index.js
@@ -2,7 +2,7 @@ const today = new Date();
 const thisYear = today.getFullYear();
 const footer = document.querySelector('footer');
 const copyright = document.createElement('p');
-copyright.innerHTML = ` Oksana Feterovskaya &copy ${thisYear}`;
+copyright.innerHTML = ` Oksana Feterovskaya &copy; ${thisYear}`;
 footer.appendChild(copyright);
 
 // add sticky header
@@ -20,7 +20,7 @@ function myFunction() {
 const skills = ['HTML', 'CSS', 'JavaScript','Git Hub', 'UI/UX','Wordpress','Adobe Visual', 'Adobe Photoshop', 'Adobe Illustrator', ];
 const skillsSection = document.getElementById("skills");
 const skillsList = skillsSection.querySelector('ul');
-for (let i = 0; i < skills.length; i++){
+for (let i = 0; i <skills.length; i++){
     let skill = document.createElement('li');
     skill.innerHTML = skills[i];
     skillsList.appendChild(skill)
@@ -67,6 +67,7 @@ const removeButton = document.createElement('button');
 // hide Message section till message is created and submit button pressed 
         if (messageList.childElementCount === 0) {
             messageSection.style.display = "none";
+            console.log(messageList.childElementCount)
         }
     });
   
@@ -109,9 +110,10 @@ fetch ("https://api.github.com/users/ofeterovskaya/repos")
     .then ((repositories) =>{
         let projectSection = document.getElementById("projects");
         let projectList = projectSection.querySelector('ul');
-        for(let i = 0; i < repositories.length; i++) {        
+        for(let i = 0; i <repositories.length; i++) {        
             const project = document.createElement ('li');
-            project.innerHTML += `<a href = "${repositories[i].html_url}" target="_blank"> ${repositories[i].name}  </a>`;
+            project.innerHTML += `<a href = "${repositories[i].html_url}" target="_blank"> ${repositories[i].name}</a>`;
             projectList.appendChild(project);
         }
-    })
+    }
+    )
